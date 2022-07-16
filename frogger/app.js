bca9e2a0df92fca32368b63232ac94740e8bf01a
João Guilherme Paiva <38386145+jguilhermepaiva@users.noreply.github.com>
2022-07-16 02:26:16 -0300

    frog-one

---
+++ frogger/app.js
@@ -0,0 +1,34 @@
+const timeLeftDisplay = document.querySelector('#time-left')
+const resultDisplay = document.querySelector('#result')
+const StartPauseButton = document.querySelector('#start-pause-button')
+const squares = document.querySelectorAll('.grid div')
+console.log(squares)
+
+let currentIndex = 76
+const width = 9
+
+function moveFrog(e){
+    squares[currentIndex].classList.remove('frog')
+    switch(e.key){
+        case 'ArrowLeft':
+            console.log('move left')
+            if(currentIndex % width !==0) currentIndex -=1
+        break
+        case 'ArrowRight':
+            console.log('move right')
+            if(currentIndex % width < width-1) currentIndex +=1
+        break
+        case 'ArrowUp':
+            console.log('move up')
+            if(currentIndex >= width) currentIndex -= width
+        break
+        case 'ArrowDown':
+            console.log('move down')
+            if(currentIndex + width < width*width) currentIndex += width
+        break
+    }
+
+    squares[currentIndex].classList.add('frog')
+}
+
+document.addEventListener('keyup', moveFrog)
